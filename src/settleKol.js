e2f475aac673685c2574f795c071d5f61bfb4d80
tunogya <tunogya@qq.com>
2023-05-08 15:39:23 +0800

    update

--- src/settleKol.js
+++ src/settleKol.js
@@ -8,44 +8,64 @@ class Main {
   
   }
   
-  async settle(date) {
-    // 1. 找到 date 当天所有的未被结算的清算表记录
+  checkDate(date) {
     if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
       console.log('date format error')
-      return
+      return false
     }
+    return true
+  }
+  
+  async settlementFutureKOL(date, chainId) {
+    const settlementMap = {}
     const clearingOrders = await knexInstance('b_clearing_kol')
         .where('date', date)
         .where('status', true)
         .where('settlementStatus', false)
-    console.log('--find clearingOrders:', clearingOrders.length)
-    const settlementMap = {}
+        .where('chainId', chainId)
     if (clearingOrders.length === 0) {
       console.log('no clearingOrders to be settled')
-      return
+      return {}
     }
     for (let i = 0; i < clearingOrders.length; i++) {
-      const {walletAddress, reward, chainId} = clearingOrders[i]
+      const {walletAddress, reward} = clearingOrders[i]
       if (!settlementMap[walletAddress]) {
         settlementMap[walletAddress] = {
           settlementAmount: 0,
           settlementCurrency: 'NEST',
           chainId,
+          type: 'future'
         }
       }
       settlementMap[walletAddress].settlementAmount += reward
     }
+    return settlementMap
+  }
+  
+  async settle(date) {
+    // 1. 找到 date 当天所有的未被结算的清算表记录
+    if (!this.checkDate(date)) {
+      return
+    }
+    
+    const settlementFutureKOLMap = await this.settlementFutureKOL(date, 56)
+    
+    if (Object.keys(settlementFutureKOLMap).length === 0) {
+      return
+    }
+    
     // 插入结算表，更新清算表，使用事务
     const trx = await knexInstance.transaction()
     try {
-      const settlementOrders = Object.keys(settlementMap).map(walletAddress => {
-        const {settlementAmount, settlementCurrency, chainId} = settlementMap[walletAddress]
+      const settlementOrders = Object.keys(settlementFutureKOLMap).map(walletAddress => {
+        const {settlementAmount, settlementCurrency, chainId, type} = settlementFutureKOLMap[walletAddress]
         return {
           walletAddress,
           settlementAmount,
           settlementCurrency,
           chainId,
           date,
+          type
         }
       })
       await trx('b_settlement').insert(settlementOrders)
@@ -61,6 +81,7 @@ class Main {
       await trx.rollback()
     }
   }
+  
   async start() {
     let yesterday = new Date()
     yesterday.setDate(yesterday.getDate() - 1)
