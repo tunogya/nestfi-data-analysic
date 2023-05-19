88c62a729c6c6d876327c19c952e623e0ca0bf64
tunogya <tunogya@qq.com>
2023-05-19 18:00:30 +0800

    update

--- src/lib/tx/execute.js
+++ src/lib/tx/execute.js
@@ -5,9 +5,9 @@ import knexInstance from "../db.js";
 const handleExecute = async (tx, chainId) => {
   const {blockNumber, gasFee, hash, status, timeStamp, walletAddress} = getDataFromTx(tx);
   // execute(uint256[7] prices,uint256[] orderIndices)
-  const ethprice = BigNumber.from('0x' + tx.input.slice(10, 74)).div(BigNumber.from(10).pow(14)).toNumber() / 10000;
-  const btcprice = BigNumber.from('0x' + tx.input.slice(74, 138)).div(BigNumber.from(10).pow(14)).toNumber() / 10000;
-  const bnbprice = BigNumber.from('0x' + tx.input.slice(138, 202)).div(BigNumber.from(10).pow(14)).toNumber() / 10000;
+  const ethprice = BigNumber.from('0x' + tx.input.slice(10, 74)).div(BigNumber.from(10).pow(12)).toNumber() / 1000000;
+  const btcprice = BigNumber.from('0x' + tx.input.slice(74, 138)).div(BigNumber.from(10).pow(12)).toNumber() / 1000000;
+  const bnbprice = BigNumber.from('0x' + tx.input.slice(138, 202)).div(BigNumber.from(10).pow(12)).toNumber() / 1000000;
   
   let maticprice = 0, adaprice = 0, dogeprice = 0, xrpprice = 0;
   if (blockNumber >= 28341342) {
