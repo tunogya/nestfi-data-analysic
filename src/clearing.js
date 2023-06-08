ed1ac5e0707f6b0121c9c9995c3f101d2e2fce11
tunogya <tunogya@qq.com>
2023-06-08 16:47:47 +0800

    update

--- src/clearing.js
+++ src/clearing.js
@@ -191,18 +191,18 @@ class Clearing {
 (async () => {
   const bsc = new Clearing(56);
   // const scroll = new ClearingFuture(534353);
-  
-  bsc.handleYesterday().then(() => {
-    console.log('bsc executed finally:' + new Date());
-  })
+
+  // bsc.handleYesterday().then(() => {
+  //   console.log('bsc executed finally:' + new Date());
+  // })
   // scroll.handleYesterday().then(() => {
   //   console.log('scroll executed finally:' + new Date());
   // })
+  
+  bsc.handleHistory().catch(e => {
+    console.log('clearing history error', e)
+  }).finally(() => {
+    console.log('executed finally:' + new Date())
+    process.exit(0)
+  })
 })();
-
-// clearing.handleHistory().catch(e => {
-//   console.log('clearing history error', e)
-// }).finally(() => {
-//   console.log('executed finally:' + new Date())
-//   process.exit(0)
-// })
